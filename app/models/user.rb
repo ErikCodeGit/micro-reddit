3dcab50dbf8dbea550d0fd7a8a612f9618770146
ErikCodeGit <erik.mueller0912@gmail.com>
2023-02-04 11:31:04 +0100

    Finish Lesson

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  validates :username, presence: true, length: { in: 3..20 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..32 }
 end
